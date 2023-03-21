284f8a245890072f3733e71a864796c7555c2b0b
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-21 17:48:25 -0300

    feat: página de um post

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -1,18 +1,44 @@
 <script setup lang="ts">
+const route = useRoute();
+const paramId = parseInt(route.params.id as string);
+
+if (isNaN(paramId)) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Página não encontrada",
+  });
+}
+
 const postList = await getPosts();
 
-const route = useRoute();
-const paramId = route.params.id;
+if (!postList.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Página não encontrada",
+  });
+}
 
 /* Aqui é importante usar `filter` em vez de postList.value[postId - 1]
   por que não temos certeza que a ordem dos posts vai ser sempre a mesma
 */
-// TODO: Certificar que paramId é um número
-const post = postList.value.filter((post) => post.id == paramId);
+const post = postList.value.filter((post) => post.id == paramId)[0];
 
-// TODO: Mandar usuário para página 404 se não focar encontrado o post
+if (!post) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Página não encontrada",
+  });
+}
+
+// Buscamos os comentários só depois de verificar que o post é valido
+const commentList = await getComments(paramId);
 </script>
 
 <template>
   <h2>{{ post }}</h2>
+  <ul>
+    <li v-for="comment in commentList">
+      {{ comment.email }}
+    </li>
+  </ul>
 </template>
