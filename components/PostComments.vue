6c5cb4c86ebd1512ff4a05bc31439fc88db86cbb
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-22 14:16:13 -0300

    style: estilos para comentários

--- components/PostComments.vue
+++ components/PostComments.vue
@@ -13,11 +13,35 @@ const { error, pending, commentList } = getComments(props.postId);
   <div v-else-if="commentList?.length == 0">Nada aqui</div>
   <div v-else>
     <ul>
-      <li v-for="comment in commentList">
-        <h3>{{ comment.name }}</h3>
-        <p>{{ comment.email }}</p>
-        <p>{{ comment.body }}</p>
+      <li class="comment" v-for="comment in commentList">
+        <h3 class="comment__title">{{ comment.name }}</h3>
+        <p class="comment__email">{{ comment.email }}</p>
+        <p class="comment__body">{{ comment.body }}</p>
       </li>
     </ul>
   </div>
 </template>
+
+<style lang="scss">
+.comment {
+  margin-top: 1.5rem;
+}
+
+.comment__title {
+  line-height: 1.1;
+  font-size: var(--step-0);
+}
+
+.comment__body::first-letter,
+.comment__title::first-letter {
+  text-transform: uppercase;
+}
+
+.comment__email {
+  text-transform: lowercase;
+}
+
+.comment__body {
+  margin-top: 0.2rem;
+}
+</style>
