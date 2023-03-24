b1c2bb3b5851c0200628795c12aaca04a5972878
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-24 11:43:28 -0300

    feat: meta tag de description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   app: {
     head: {
       htmlAttrs: { lang: "pt-BR" },
+      meta: [
+        {
+          name: "description",
+          content:
+            "O Alkablog é o resultado de um teste técnico de um processo seletivo. ",
+        },
+      ],
     },
   },
   modules: ["unplugin-icons/nuxt"],
